ab2b212907a8eda3176ead54a7316980693316ec
hfelicjaniak <hfelicjaniak@edu.cdv.pl>
2021-01-08 11:15:01 +0100

    skonczona plansza i menu gl

---
+++ Assets/Scripts/SliderText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SliderText : MonoBehaviour
+{
+
+    TextMeshProUGUI textComponent;
+
+    void Start()
+    {
+        textComponent = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void SetSliderValue(float sliderValue)
+    {
+        int rounded_f = (int)(sliderValue*100 + 0.5f);
+        textComponent.text = (rounded_f).ToString();
+    }
+}
